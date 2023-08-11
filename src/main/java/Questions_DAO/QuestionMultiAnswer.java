986d6374559a89be3e1272c2fa225d1135b4e81d
dgelashvili <dgela21@freeuni.edu.ge>
2023-08-11 13:31:11 +0400

    added all question classes left

---
+++ src/main/java/Questions_DAO/QuestionMultiAnswer.java
@@ -0,0 +1,104 @@
+package Questions_DAO;
+
+import Questions_DAO.Question;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+import static Questions_DAO.QuestionMultiChoice.getStrings;
+
+
+public class QuestionMultiAnswer implements Question {
+    private final String question;
+    private final ArrayList<String> answers;
+    private final boolean ordered;
+    private final boolean caseSensitive;
+    public QuestionMultiAnswer(String question, String answer, boolean ordered, boolean caseSensitive) {
+        this.question = question;
+        this.ordered = ordered;
+        this.caseSensitive = caseSensitive;
+        this.answers = fillList(answer);
+    }
+
+    @Override
+    public int getType() {
+        return 5;
+    }
+
+    @Override
+    public String getQuestion() {
+        return question;
+    }
+
+    @Override
+    public ArrayList<String> getTexts() {
+        return null;
+    }
+
+    @Override
+    public String getImage() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<String> getPossibleAnswers() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    @Override
+    public boolean isOrdered() {
+        return ordered;
+    }
+
+    @Override
+    public boolean isCaseSensitive() {
+        return caseSensitive;
+    }
+
+    @Override
+    public int checkAnswer(ArrayList<String> user_answers) {
+        if (user_answers.size() > getAnswers().size()) { return 0; }
+
+        if (isOrdered()) {
+            int counter = 0;
+            for (int i = 0; i < user_answers.size(); i++) {
+                if ((isCaseSensitive() && user_answers.get(i).equals(getAnswers().get(i))) ||
+                        (!isCaseSensitive() && user_answers.get(i).toUpperCase().equals(getAnswers().get(i)))) {
+                    counter++;
+                }
+            }
+            return counter;
+        } else {
+            HashSet<String> s = new HashSet<>();
+
+            if (isCaseSensitive()) { s.addAll(user_answers); }
+            else {
+                for (String userAnswer : user_answers) {
+                    s.add(userAnswer.toUpperCase());
+                }
+            }
+            int sz = s.size();
+            s.addAll(getAnswers());
+            return sz + getAnswers().size() - s.size();
+        }
+    }
+
+    @Override
+    public int getMaxScore() {
+        return getAnswers().size();
+    }
+
+    private ArrayList<String> fillList(String answer) {
+        if (!isCaseSensitive()) { answer = answer.toUpperCase(); }
+
+        ArrayList<String> ls = getStrings(answer);
+        if (!isOrdered()) { Collections.sort(ls); }
+        return ls;
+    }
+}
---
+++ src/main/java/Questions_DAO/QuestionPictureResponse.java
@@ -0,0 +1,80 @@
+package Questions_DAO;
+
+import java.util.ArrayList;
+
+public class QuestionPictureResponse implements Question {
+    private final String question;
+    private final String image;
+    private final String answer;
+    private final boolean ordered;
+    private final boolean caseSensitive;
+    public QuestionPictureResponse(String question, String image, String answer, boolean ordered, boolean caseSensitive) {
+        this.question = question;
+        this.image = image;
+        this.answer = answer;
+        this.ordered = ordered;
+        this.caseSensitive = caseSensitive;
+    }
+    @Override
+    public int getType() {
+        return 4;
+    }
+
+    @Override
+    public String getQuestion() {
+        return question;
+    }
+
+    @Override
+    public ArrayList<String> getTexts() {
+        return null;
+    }
+
+    @Override
+    public String getImage() {
+        return image;
+    }
+
+    @Override
+    public ArrayList<String> getPossibleAnswers() {
+        ArrayList<String> ls = new ArrayList<>();
+        ls.add(image);
+        return ls;
+    }
+
+    @Override
+    public ArrayList<String> getAnswers() {
+        ArrayList<String> answers = new ArrayList<>();
+
+        if (isCaseSensitive()) {
+            answers.add(answer);
+        } else {
+            answers.add(answer.toUpperCase());
+        }
+
+        return answers;
+    }
+
+    @Override
+    public boolean isOrdered() {
+        return ordered;
+    }
+
+    @Override
+    public boolean isCaseSensitive() {
+        return caseSensitive;
+    }
+
+    @Override
+    public int checkAnswer(ArrayList<String> user_answers) {
+        if (user_answers.size() > getAnswers().size() || user_answers.size() == 0) { return 0; }
+        if ((isCaseSensitive() && user_answers.get(0).equals(getAnswers().get(0))) ||
+                (!isCaseSensitive() && user_answers.get(0).toUpperCase().equals(getAnswers().get(0)))) { return 1; }
+        return 0;
+    }
+
+    @Override
+    public int getMaxScore() {
+        return getAnswers().size();
+    }
+}
