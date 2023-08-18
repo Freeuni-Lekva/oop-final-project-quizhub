fa533f0ffe1ef4ee199bcf9287a3a564f2994647
Luka <lmebo21@freeuni.edu.ge>
2023-08-18 22:38:44 +0400

    added quiz database files

--- src/main/java/DATABASE_DAO/QuizDatabases/QuizDatabase.java
+++ src/main/java/DATABASE_DAO/QuizDatabases/QuizDatabase.java
@@ -1,28 +1,44 @@
 package DATABASE_DAO.QuizDatabases;
 
+import DATABASE_DAO.ConnectionPool;
 import DATABASE_DAO.Database;
+import Questions_DAO.Question;
 import Questions_DAO.Quiz;
+import javafx.util.Pair;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class QuizDatabase extends Database {
 
     public static String tablename = "QuizTable";
 
+    private String databaseNmae = "my_database;";
+
     public QuizDatabase() throws SQLException {
-        super();
+    }
+
+    public QuizDatabase(String under_score) throws SQLException{
+        super(under_score);
+        databaseNmae = "test_database;";
     }
 
     public String getQuizName(int id) throws SQLException {
+        Connection connection = ConnectionPool.openConnection();
+        String result = "";
         Statement statement = connection.createStatement();
-        statement.execute("USE " + "my_database;");
+        statement.execute("USE " + databaseNmae);
         ResultSet resultSet = statement.executeQuery("SELECT* FROM " + tablename + " WHERE id =" + id + ";");
         if(resultSet.next()){
-            return resultSet.getString(2);} return "";
+            result = resultSet.getString(2);
+        }
+        ConnectionPool.closeConnection(connection);
+        return result;
     }
 
     public void addQuiz(int id, String name, String creator, String category, String description, boolean random, boolean onePage, boolean immediateCorrection, boolean practiceMode) throws SQLException {
+        Connection connection = ConnectionPool.openConnection();
         String insertStatement = "INSERT INTO " + tablename + " (id, name, creator, category, description,create_date, random, onePage, immediateCorrection, practiceMode) " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement preparedStatement = connection.prepareStatement(insertStatement);
@@ -36,14 +52,16 @@ public class QuizDatabase extends Database {
         preparedStatement.setBoolean(8, onePage);
         preparedStatement.setBoolean(9, immediateCorrection);
         preparedStatement.setBoolean(10, practiceMode);
-        preparedStatement.execute("USE " + "my_database;");
+        preparedStatement.execute("USE " + databaseNmae);
         preparedStatement.executeUpdate();
+        ConnectionPool.closeConnection(connection);
     }
 
     public Quiz getQuiz(int id) throws SQLException {
+        Connection connection = ConnectionPool.openConnection();
         Quiz quiz = null;
         Statement statement = connection.createStatement();
-        statement.execute("USE " + "my_database;");
+        statement.execute("USE " + databaseNmae);
         ResultSet resultSet = statement.executeQuery("SELECT* FROM " + tablename + " WHERE id =" + id + ";");
         if(resultSet.next()) {
             quiz = new Quiz(resultSet.getString(2), resultSet.getString(3),
@@ -51,7 +69,42 @@ public class QuizDatabase extends Database {
                     resultSet.getBoolean(7), resultSet.getBoolean(8),
                     resultSet.getBoolean(9), resultSet.getBoolean(10));
         }
+        ConnectionPool.closeConnection(connection);
         return quiz;
     }
 
+    public ArrayList<Integer> getAllQuizes() throws SQLException {
+        Connection connection = ConnectionPool.openConnection();
+        ArrayList<Integer> ls = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        statement.execute("USE " + databaseNmae);
+        ResultSet resultSet = statement.executeQuery("SELECT id FROM " + tablename + ";");
+        while(resultSet.next()) {
+            ls.add(resultSet.getInt("id"));
+        }
+        ConnectionPool.closeConnection(connection);
+        return ls;
+    }
+
+    public ArrayList<Pair<Quiz,Integer>> getFoundQuizByName(String name) throws SQLException {
+        Connection connection = ConnectionPool.openConnection();
+        TagsQuizDatabase tagsQuizDatabase = new TagsQuizDatabase();
+        QuizQuestionDatabase quizQuestionDatabase = new QuizQuestionDatabase();
+        ArrayList<Pair<Quiz,Integer>> list = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        statement.execute("USE " + databaseNmae);
+        ResultSet resultSet = statement.executeQuery("SELECT* FROM " + tablename + " WHERE name LIKE '%" + name + "%';");
+        while(resultSet.next()){
+            int id = resultSet.getInt("id");
+            Quiz quiz1 = getQuiz(id);
+            ArrayList<String> tags = tagsQuizDatabase.getTags(id);
+            ArrayList<Question> questions = quizQuestionDatabase.getQuestions(id);
+            Quiz quiz = new Quiz(quiz1.getQuizName(), quiz1.getCreatorName(), quiz1.getCategory(), quiz1.getDescription(),
+                    tags, questions, quiz1.isRandom(), quiz1.isOnePage(), quiz1.hasImmediateCorrection(), quiz1.isPracticeMode());
+            Pair<Quiz, Integer> pair = new Pair<>(quiz, id);
+            list.add(pair);
+        }
+        ConnectionPool.closeConnection(connection);
+        return list;
+    }
 }
--- src/main/java/DATABASE_DAO/QuizDatabases/TagsQuizDatabase.java
+++ src/main/java/DATABASE_DAO/QuizDatabases/TagsQuizDatabase.java
@@ -1,5 +1,6 @@
 package DATABASE_DAO.QuizDatabases;
 
+import DATABASE_DAO.ConnectionPool;
 import DATABASE_DAO.Database;
 import org.apache.commons.dbcp2.BasicDataSource;
 
@@ -10,32 +11,53 @@ public class TagsQuizDatabase extends Database {
 
     public static String tablename = "Tags_QuizTable";
 
+    private String databaseName = "my_database;";
+
     public TagsQuizDatabase() throws SQLException {
-        super();
+    }
+
+    public TagsQuizDatabase(String under_score) throws SQLException{
+        super(under_score);
+        databaseName = "test_database;";
     }
 
     public void addQuiz(String tags, int quiz_id) throws SQLException {
+        Connection connection = ConnectionPool.openConnection();
         String insertStatement = "INSERT INTO " + tablename + " (tags, id) " +
                 "VALUES (?, ?)";
         PreparedStatement preparedStatement = connection.prepareStatement(insertStatement);
         preparedStatement.setString(1, tags);
         preparedStatement.setInt(2, quiz_id);
-        preparedStatement.execute("USE " + "my_database;");
+        preparedStatement.execute("USE " + databaseName);
         preparedStatement.executeUpdate();
+        ConnectionPool.closeConnection(connection);
     }
 
     public ArrayList<String> getTags(int quiz_id) throws SQLException {
+        Connection connection = ConnectionPool.openConnection();
         ArrayList<String> list = new ArrayList<>();
         Statement statement = connection.createStatement();
-        statement.execute("USE " + "my_database");
+        statement.execute("USE " + databaseName);
         ResultSet resultSet = statement.executeQuery("SELECT tags FROM " + tablename + " WHERE id =" + quiz_id + ";");
         while(resultSet.next()){
             list.add(resultSet.getString("tags"));
         }
+        ConnectionPool.closeConnection(connection);
         return list;
     }
 
 
-
-
+    public ArrayList<Integer> getFoundQuizByTag(String tag) throws SQLException {
+        Connection connection = ConnectionPool.openConnection();
+        ArrayList<Integer> list = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        statement.execute("USE " + databaseName);
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM " + tablename + " WHERE tags LIKE '%" + tag + "%';");
+        while(resultSet.next()){
+            int id = resultSet.getInt("id");
+            list.add(id);
+        }
+        ConnectionPool.closeConnection(connection);
+        return list;
+    }
 }
