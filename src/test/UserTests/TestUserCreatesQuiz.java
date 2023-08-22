b8f8e9764a88fdc4b611803bbaf08697ac5d5dff
vardohan1 <138012032+vardohan1@users.noreply.github.com>
2023-08-22 14:04:24 +0400

    Update TestUserCreatesQuiz.java

--- src/test/UserTests/TestUserCreatesQuiz.java
+++ src/test/UserTests/TestUserCreatesQuiz.java
@@ -31,6 +31,7 @@ public class TestUserCreatesQuiz extends TestCase {
         quizDatabase = new QuizDatabase();
         quizQuestionDatabase = new QuizQuestionDatabase();
         tagsQuizDatabase = new TagsQuizDatabase();
+        quizDatabase.clearAllTables();
     }
 
     @Test
